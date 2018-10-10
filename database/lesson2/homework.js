10892386c5a8ddcb7a82ca64d8584add2eea8624
nirmala <nirmala@surf.net.np>
2018-10-10 21:20:34 +0200

    add lesson2 folder, homework.js file

---
+++ database/lesson2/homework.js
@@ -0,0 +1,20 @@
+const addNewTask = function(title, description, created, updated, dueDate, statusID, userID) {
+};
+
+const changeTaskTitle = function(taskID, newTitle) {
+
+};
+
+const changeTaskDueDate = function(taskID, newDueDate) {
+};
+
+const changeTaskStatus = function(taskID, newStatus) {
+};
+
+const markTaskAsCompleted = function(taskID) {
+};
+
+const deleteTask = function(taskID) {
+};
+
+
