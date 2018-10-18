07374e8dc7a0ff5ed5ec0c1a05e295807ff9d8fb
nirmala <nirmala@surf.net.np>
2018-10-18 20:08:53 +0200

    add row contents in tables

--- database/lesson3/music.sql
+++ database/lesson3/music.sql
@@ -4,6 +4,11 @@ CREATE TABLE `artists` (
 	`album_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+insert into artists(artist_id, artist_name, album_name) values(1, 'Orella Raccio', 'Savannah deer');
+insert into artists(artist_id, artist_name, album_name) values(2, 'Blake Dreakin', 'Black rhinoceros');
+insert into artists(artist_id, artist_name, album_name) values(3, 'Dolph Mendus', 'Bunting');
+insert into artists(artist_id, artist_name, album_name) values(4, 'Freddi', 'frilled');
+
 CREATE TABLE `albums` (
 	`album_id` INT(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`album_name` varchar(255) NOT NULL,
@@ -11,6 +16,9 @@ CREATE TABLE `albums` (
 	`artist_id` INT(11) unsigned NOT NULL,
   CONSTRAINT `fk_artist` FOREIGN KEY (`artist_id`) REFERENCES `artists`(`artist_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert into albums(album_id, album_name, realese_date, artist_id) values(1, 'crazy world', '2018-10-09', 1);
+insert into albums(album_id, album_name, realese_date, artist_id) values(2, 'Hot chilli peppers', '2015-10-09', 2);
 
 CREATE TABLE `song_tracks` (
     `id` INT(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
