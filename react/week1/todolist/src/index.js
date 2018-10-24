b81899c1510731a722fcde5c6a177bfa5c0a45aa
nirmala <nirmala@surf.net.np>
2018-10-24 22:07:03 +0200

    add static todolist

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+
+const todoItems = [
+  {
+    item: "Get out of bed", 
+    date: new Date(2017, 8, 13).toDateString()
+  },
+  {
+    item: "Brush teeth", 
+    date: new Date(2017, 8, 14).toDateString()
+  },
+  {
+    item: "Eat breakfast", 
+    date: new Date(2017, 8, 15).toDateString()
+  }
+];
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        <h2>todolist</h2>
+        <List/>
+      </div>
+    );
+  }
+}
+
+class List extends React.Component{
+  render(){    
+    return(
+      <div className="list">
+         { todoItems.map((el) => {
+              return <Item todo={`${el.item}, `} date={el.date}/>
+          })}
+      </div>
+    );
+  }
+}
+
+class Item extends React.Component{
+  render(){
+    return(
+      <div>
+        <li>{this.props.todo}{this.props.date}</li>
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
