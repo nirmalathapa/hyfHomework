6e6ef8c12b6cbdf66e72e60b031b3397953014c0
nirmala <nirmala@surf.net.np>
2018-10-26 23:50:03 +0200

    add id the array

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -4,14 +4,17 @@ import './index.css'
 
 const todoItemsCollection = [
   {
+    id:1,
     item: "Get out of bed", 
     date: new Date(2017, 8, 13).toDateString()
   },
   {
+    id:2,
     item: "Brush teeth", 
     date: new Date(2017, 8, 14).toDateString()
   },
   {
+    id:3,
     item: "Eat breakfast", 
     date: new Date(2017, 8, 15).toDateString()
   }
@@ -21,8 +24,8 @@ class App extends React.Component{
   render(){
     return(
       <div className="container">
-        <h2>todolist</h2>
-        <List/>
+        <h1>todolist</h1>
+        <List />
       </div>
     );
   }
@@ -33,7 +36,7 @@ class List extends React.Component{
     return(
       <div className="list">
          { todoItemsCollection.map((el) => {
-              return <Item todoItem={`${el.todoItem},`} date={el.date}/>
+              return <Item key={el.id} todoItem={`${el.item}, `} date={el.date}/>
           })}
       </div>
     );
