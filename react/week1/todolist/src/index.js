e69e605832a01167595036c3045fdedb0b7d357e
nirmala <nirmala@surf.net.np>
2018-10-29 13:28:53 +0100

    Delete unwanted files and add separate components

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import TodoList from './todo/TodoList'
 
-const todoItemsCollection = [
-  {
-    id:1,
-    item: "Get out of bed", 
-    date: new Date(2017, 8, 13).toDateString()
-  },
-  {
-    id:2,
-    item: "Brush teeth", 
-    date: new Date(2017, 8, 14).toDateString()
-  },
-  {
-    id:3,
-    item: "Eat breakfast", 
-    date: new Date(2017, 8, 15).toDateString()
-  }
-];
+let todoList = [
+    {id: 1, todo: "Get out of bed,", finishDate: " Wed Sep 13 2017"}, 
+    {id: 2, todo: "Brush teeth,", finishDate: " Thu Sep 14 2017"}, 
+    {id: 3, todo: "Eat breakfast,", finishDate: " Fri Sep 15 2017"}
+  ];
 
 class App extends React.Component{
   render(){
     return(
       <div className="container">
         <h1>todolist</h1>
-        <List />
+        <TodoList todoList={todoList}/>
       </div>
     );
   }
 }
-
-class List extends React.Component{
-  render(){    
-    return(
-      <div className="list">
-         { todoItemsCollection.map((el) => {
-              return <Item key={el.id} todoItem={`${el.item}, `} date={el.date}/>
-          })}
-      </div>
-    );
-  }
-}
-
-class Item extends React.Component{
-  render(){
-    return(
-      <div>
-        <li>{this.props.todoItem}{this.props.date}</li>
-      </div>
-    );
-  }
-}
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
