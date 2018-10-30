de10e656916e4e6ece810e5fbd49b3e94d22a7ee
nirmala <nirmala@surf.net.np>
2018-10-30 23:02:26 +0100

    make todolist json data

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -4,9 +4,9 @@ import './index.css'
 import TodoList from './todo/TodoList'
 
 let todoList = [
-    {id: 1, todo: "Get out of bed,", finishDate: " Wed Sep 13 2017"}, 
-    {id: 2, todo: "Brush teeth,", finishDate: " Thu Sep 14 2017"}, 
-    {id: 3, todo: "Eat breakfast,", finishDate: " Fri Sep 15 2017"}
+    {"id": 1, "todo": "Get out of bed,", "finishDate": " Wed Sep 13 2017", "done": true}, 
+    {"id": 2, "todo": "Brush teeth,", "finishDate": " Thu Sep 14 2017"}, 
+    {"id": 3, "todo": "Eat breakfast,", "finishDate": " Fri Sep 15 2017"}
   ];
 
 class App extends React.Component{
