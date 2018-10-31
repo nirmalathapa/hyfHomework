55529249a675f1e90d780839c3af9a0d74316e25
nirmala <nirmala@surf.net.np>
2018-10-31 22:58:01 +0100

    add todo in each todoitem

--- react/week1/todolist/src/todo/TodoList.js
+++ react/week1/todolist/src/todo/TodoList.js
@@ -7,7 +7,7 @@ const TodoList = (props) => {
     <ul>
       {
         props.todoList.map((todo) => {
-          return <TodoItem key={todo.id} todo= {todo.todo} finishDate={todo.finishDate}/>
+          return <TodoItem key={todo.id} todo= {todo.todo} finishDate={todo.finishDate} done={todo.done}/>
         })
       }
     </ul>
