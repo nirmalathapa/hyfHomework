044dd5ee80c329bbb1cb6f52520b5cde934894bf
nirmala <nirmala@surf.net.np>
2018-11-03 00:01:45 +0100

    add delete button on todoitem

--- react/week1/todolist/src/todo/TodoItem.js
+++ react/week1/todolist/src/todo/TodoItem.js
@@ -4,24 +4,29 @@ class TodoItem extends React.Component {
   constructor(props) {
     super(props);
     this.handleComplete = this.handleComplete.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleComplete() {
     this.props.onToggle(this.props.id);
   }
 
+  handleDelete() {
+    this.props.onDelete(this.props.id);
+  }
+
   render() {
     return (
-      <li>
+      <li>        
         <input
           type="checkbox"
           checked={this.props.done}
           onChange={this.handleComplete}
         />
-
-        <span className={this.props.done  === true ? "done" : "not-done"}>
+        <span className={this.props.done === true ? "done" : "not-done"}>
           {this.props.todo} {this.props.finishDate}
         </span>
+        <button onClick={this.handleDelete}>X</button>
       </li>
     );
   }
