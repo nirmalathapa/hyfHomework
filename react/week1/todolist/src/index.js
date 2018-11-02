8d10c24cafe659f1e0cadb194d995414b088ebfe
nirmala <nirmala@surf.net.np>
2018-11-02 23:59:46 +0100

    add ontoggle, ondelete, onadd function

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -27,22 +27,49 @@ const todoList = [
 class App extends React.Component {
   constructor(props) {
     super(props);
-    console.log({todoList});
-    this.state = {todoList:  todoList}
+    this.state = { todoList: todoList, value: '' };
   }
 
-  handleToggle = (id) => {
-    const foundIndex = this.state.todoList.findIndex(item => item.id === id)
-    const newTodoList = [...this.state.todoList]
-    newTodoList[foundIndex].done = !newTodoList[foundIndex].done
-    this.setState({todoList: newTodoList})
+  handleToggle = id => {
+    const foundIndex = this.state.todoList.findIndex(item => item.id === id);
+    const newTodoList = [...this.state.todoList];
+    newTodoList[foundIndex].done = !newTodoList[foundIndex].done;
+    this.setState({ todoList: newTodoList });
+  };
+
+  handleDelete = id => {
+    const index = this.state.todoList.findIndex(item => item.id === id);
+    const newTodoList = [...this.state.todoList];
+    newTodoList.splice(index, 1);
+    this.setState({ todoList: newTodoList });
+  };
+
+  handleChangeValue = (e) => {
+    this.setState({value: e.target.value});
   }
 
+  handleAddTodo = () => {
+    const arr = this.state.todoList;
+    arr.push({id:Math.random(1), todo:this.state.value});    
+    this.setState({todoList: arr})
+    console.log(arr);    
+  }
   render() {
     return (
       <div className="container">
         <h1>todolist</h1>
-        <TodoList todoList={this.state.todoList} onToggle={this.handleToggle} />
+          <input type="text" onChange={this.handleChangeValue} value={this.state.value}/>
+          <button className="addButton" onClick={this.handleAddTodo} >Add todo</button>
+        {this.state.todoList.length === 0 ? (
+          "No todos left"
+        ) : (
+          <TodoList
+            todoList={this.state.todoList}
+            onToggle={this.handleToggle}
+            onDelete={this.handleDelete}
+            onAdd={this.handleAddTodo}
+          />
+        )}
       </div>
     );
   }
