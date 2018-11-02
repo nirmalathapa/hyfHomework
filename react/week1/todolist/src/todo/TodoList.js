952784e1b7ce5164e3527320bf2b4d7dc4af9d25
nirmala <nirmala@surf.net.np>
2018-11-03 00:02:28 +0100

    add delete funtion in todolist

--- react/week1/todolist/src/todo/TodoList.js
+++ react/week1/todolist/src/todo/TodoList.js
@@ -2,23 +2,26 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 class TodoList extends React.Component {
-  render() {
 
+  handleChangeValue(e) {
+  }
+  render() {
     return (
-      <ul>
-        {this.props.todoList.map(todo => {
-          return (
-            <TodoItem
-              key={todo.id}
-              id={todo.id}
-              todo={todo.todo}
-              finishDate={todo.finishDate}
-              done={todo.done}
-              onToggle={this.props.onToggle}
-            />
-          );
-        })}
-      </ul>
+        <ul>
+          {this.props.todoList.map(todo => {
+            return (
+              <TodoItem
+                key={todo.id}
+                id={todo.id}
+                todo={todo.todo}
+                finishDate={todo.finishDate}
+                done={todo.done}
+                onToggle={this.props.onToggle}
+                onDelete={this.props.onDelete}
+              />
+            );
+          })}
+        </ul>
     );
   }
 }
