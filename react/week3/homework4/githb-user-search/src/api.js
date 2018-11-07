adf74ede0c9b2a909bb6b889605bea54e60a263a
nirmala <nirmala@surf.net.np>
2018-11-07 14:31:15 +0100

    add delay function

--- react/week3/homework4/githb-user-search/src/api.js
+++ react/week3/homework4/githb-user-search/src/api.js
@@ -3,7 +3,16 @@
 const URL = "https://api.github.com/search/users";
 
 export async function getGithubUser(query) {
+  await delay(1000);
   const response = await fetch(`${URL}?q=${query}`);
   const body = await response.json();
   return body.items;
 }
+
+
+function delay(time){
+  return new Promise((resolve) => {
+    setInterval(resolve, time)
+  });
+}
+
