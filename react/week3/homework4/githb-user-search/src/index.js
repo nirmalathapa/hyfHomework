f2a2a347b5b6a1429563850f1675abff4f4f820e
nirmala <nirmala@surf.net.np>
2018-11-07 14:35:00 +0100

    add loading state

--- react/week3/homework4/githb-user-search/src/index.js
+++ react/week3/homework4/githb-user-search/src/index.js
@@ -1,52 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import SearchBox from './SearchBox';
-import Github from './Github';
-import { getGithubUser } from './api.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./components/index.css";
+import SearchBox from "./components/SearchBox";
+import Github from "./components/Github";
+import { getGithubUser } from "./api.js";
 
-
-
-class App extends React.Component{
-  constructor(props){
-  super(props);
-  this.state = {
-    users: []
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: false,
+      error: null
+    };
   }
 
-  } 
-
-  handleSearch = async (query) =>{
-    if(this.state.query === query){
+  handleSearch = async query => {
+    if (this.state.query === query){
       return;
     }
+
+    this.setState({ loading: true });
+
     this.setState({
       users: await getGithubUser(query),
-      query: query
+      query: query,
+      loading: false
     });
-  }
-  
+  };
 
-  render(){
+  render() {
     return (
       <div className="container">
         <header className="header">
           <h1>Github User Search</h1>
         </header>
         <section className="searchBox">
-          <SearchBox handleSearch={this.handleSearch}/>
-        </section>        
+          <SearchBox handleSearch={this.handleSearch} />
+        </section>
         <section className="contentDisplay">
-          <Github users={this.state.users} />
+          <Github users={this.state.users} loading={this.state.loading} />
         </section>
       </div>
     );
   }
 }
 
-
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
