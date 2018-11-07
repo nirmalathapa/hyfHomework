2e002ddc5649c6cabaaf4d25bbbce6c10e32468e
nirmala <nirmala@surf.net.np>
2018-11-07 14:32:33 +0100

    add loading message

--- react/week3/homework4/githb-user-search/src/components/Github.js
+++ react/week3/homework4/githb-user-search/src/components/Github.js
@@ -3,10 +3,8 @@ import User from "./User";
 
 export default class Github extends React.Component {
   render() {
-    const {loading, error, users} = this.props;
+    const {loading} = this.props;
     if(loading) return <p>loading..</p>;
-    else if(error) return <p>error</p>;
-    else if(users < 1) return <p>error</p>;
     else{
     return (
       <div>        
