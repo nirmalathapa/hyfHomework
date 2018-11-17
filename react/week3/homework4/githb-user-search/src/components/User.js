6384b7499a395ff1d9f46a65ada2d998bfbe090d
nirmala <nirmala@surf.net.np>
2018-11-17 15:41:24 +0100

    add image link to single user

--- react/week3/homework4/githb-user-search/src/components/User.js
+++ react/week3/homework4/githb-user-search/src/components/User.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const User = props => {
-  return (
-    <li>
-      <img src={props.user.avatar_url} alt="" />
-      <a href={props.user.html_url} target="_blank" rel="noopener noreferrer">
-        {props.user.login}
-      </a>
-    </li>
-  );
-};
-
+class User extends React.Component {
+  render() {
+    const imageLink = this.props.link;
+    return (
+      <div className="col-sm-4 mt-4 pr-2">
+        <div className="card">
+          <Link to={`/users/${imageLink}`}>
+            <img
+              className="card-img-top"
+              src={this.props.img}
+              alt="Github avatar"
+            />
+          </Link>
+          <div className="card-body">{this.props.login}</div>
+        </div>
+      </div>
+    );
+  }
+}
 export default User;
