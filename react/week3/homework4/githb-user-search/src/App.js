f3c762b4f6e58c3e819648557d3e133fc1d6ad0d
nirmala <nirmala@surf.net.np>
2018-11-17 21:05:30 +0100

    add about page route

--- react/week3/homework4/githb-user-search/src/App.js
+++ react/week3/homework4/githb-user-search/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Home from "./components/Home";
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import GithubProfile from "./components/GithubProfile";
-
+import About from "./components/About";
 
 class App extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Home}></Route>          
-          <Route exact path="/users/:id" component={GithubProfile}></Route>          
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users/:id" component={GithubProfile} />
+          <Route exact path="/about" component={About} />
         </div>
       </BrowserRouter>
     );
   }
-  
 }
 
 export default App;
