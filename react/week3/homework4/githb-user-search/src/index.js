577faffba3d459841e7888447ca2fa561d10ddf7
nirmala <nirmala@surf.net.np>
2018-11-17 15:43:03 +0100

    remove all the components from index and break into smaller components

--- react/week3/homework4/githb-user-search/src/index.js
+++ react/week3/homework4/githb-user-search/src/index.js
@@ -1,49 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./components/index.css";
-import SearchBox from "./components/SearchBox";
-import Github from "./components/Github";
-import { getGithubUser } from "./api.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      loading: false,
-      error: null
-    };
-  }
 
-  handleSearch = async query => {
-    if (this.state.query === query){
-      return;
-    }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    this.setState({ loading: true });
-
-    this.setState({
-      users: await getGithubUser(query),
-      query: query,
-      loading: false
-    });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <header className="header">
-          <h1>Github User Search</h1>
-        </header>
-        <section className="searchBox">
-          <SearchBox handleSearch={this.handleSearch} />
-        </section>
-        <section className="contentDisplay">
-          <Github users={this.state.users} loading={this.state.loading} />
-        </section>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
